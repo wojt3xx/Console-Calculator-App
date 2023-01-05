61aec618e2f0d6771e4c570f544e3d400f090b84
v01dxx <117851354+wojt3xx@users.noreply.github.com>
2023-01-05 22:52:05 +0200

    Refactoring with do while

--- ConsoleCalculatorApp/Program.cs
+++ ConsoleCalculatorApp/Program.cs
@@ -4,33 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first number: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter second number: ");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("What you want to do (sum, multiply, substract or divide)? ");
-            string userChoice = Console.ReadLine();
-
-            switch (userChoice)
+            bool quitApp = false;
+            do
             {
-                case "sum":
-                    Console.WriteLine($"{a} + {b} = " + (a + b));
-                    break;
-                case "multiply":
-                    Console.WriteLine($"{a} * {b} = " + (a * b));
-                    break;
-                case "substract":
-                    Console.WriteLine($"{a} - {b} = " + (a - b));
-                    break;
-                case "divide":
-                    Console.WriteLine($"{a} % {b} = " + (a % b));
-                    break;
-                default:
-                    Console.WriteLine("That's a weong choice. Please, try again.");
-                    break;
-            }
+                Console.WriteLine("Enter first number: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter second number: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What you want to do (sum, multiply, substract, divide or quit)? ");
+                string userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "sum":
+                        Console.WriteLine($"{a} + {b} = " + (a + b));
+                        break;
+                    case "multiply":
+                        Console.WriteLine($"{a} * {b} = " + (a * b));
+                        break;
+                    case "substract":
+                        Console.WriteLine($"{a} - {b} = " + (a - b));
+                        break;
+                    case "divide":
+                        Console.WriteLine($"{a} % {b} = " + (a % b));
+                        break;
+                    case "quit":
+                        quitApp = true;
+                        break;
+                    default:
+                        Console.WriteLine("That's a weong choice. Please, try again.");
+                        break;
+                }
+            } while (!quitApp);
         }
     }
 }
